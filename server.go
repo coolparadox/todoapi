6b170206ecf366109ca843f959263600ba4bf027
Rafael Lorandi <coolparadox@gmail.com>
2016-10-20 18:51:17 -0200

    Section: Creating Some Basic Routes
    The default library muxer does not handle "variables" in URLs, so the
    Todos handler must parse the remaining of the URL by itself in order to
    decide whether the client is requesting the index or a specific id.

--- server.go
+++ server.go
@@ -5,10 +5,13 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", Index)
+	http.HandleFunc("/todos/", Todos)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -20,4 +23,18 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func Todos(w http.ResponseWriter, r *http.Request) {
+	arg := strings.TrimPrefix(r.URL.Path, "/todos/")
+	if len(arg) == 0 {
+		fmt.Fprintln(w, "Todo Index!")
+		return
+	}
+	id, err := strconv.ParseInt(arg, 10, 32)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintln(w, "Todo show:", id)
 }
