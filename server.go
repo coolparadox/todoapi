b16d1a30f9b6d326125b00c2ec96e5098efbe5ec
Rafael Lorandi <coolparadox@gmail.com>
2016-10-20 18:03:14 -0200

    Section: Adding a Router
    Well I decided to get along with the default http ServerMux router for
    learning purposes.

--- server.go
+++ server.go
@@ -8,8 +8,16 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	http.HandleFunc("/", Index)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	// The "/" pattern matches everything, so we need to check
+	// that we're at the root here.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
