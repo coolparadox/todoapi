5482ff2323fc7b3f9fefb24e6ca7b83231833224
Rafael Lorandi <coolparadox@gmail.com>
2016-10-22 00:45:34 -0200

    Section: Applying the Logger Decorator
    Route struct is simpler at the cost of handler functions complexity.
    I've used a map with the mapping name as the index for storing routes.

--- main.go
+++ main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/todos/", handleTodos)
+	for name, route := range routes {
+		http.Handle(route.pattern, logger(route.handler, name))
+	}
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+var routes = map[string]Route{
+	"index": {"/", handleIndex},
+	"todos": {"/todos/", handleTodos},
+}
+
+type Route struct {
+	pattern string
+	handler http.HandlerFunc
+}
+
+func logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		inner.ServeHTTP(w, r)
+		log.Printf(
+			"%s\t%s\t%s\t%s",
+			r.Method,
+			r.RequestURI,
+			name,
+			time.Since(start),
+		)
+	})
 }
